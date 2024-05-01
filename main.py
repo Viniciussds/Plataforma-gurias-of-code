331f4bb1288ae618bc395ba3dce75bfe71be1435
vinisdssds <viniciussds2.aluno@unipampa.edu.br>
2024-05-01 01:08:04 -0300

    projeto-django-base

--- main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, url_for, render_template
-from rotas.home import home_rota
-# inicializar flask
-app = Flask(__name__)
-# registrando no app
-app.register_blueprint(home_rota)
-
-# modo desenvolvedor
-app.run(debug=True)
--- rotas/home.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-home_rota = Blueprint('home', __name__)
-
-@home_rota.route('/')
-def home():
-    return render_template("index.html")
